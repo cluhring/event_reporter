091fe3f25cb848032837d837d3a86781cc6f4ea5
Andrew Overby <andrewdoverby@gmail.com>
2014-11-17 14:01:11 -0700

    commit the cli file

---
+++ test/event_reporter_test.rb
@@ -0,0 +1,32 @@
+#require_relative
+require_relative '../lib/event_reporter'
+
+
+class EventReporterTest < MiniTest::Test
+
+  def test_it_does_exist
+    assert EventReporter
+  end
+
+  def test_that_help_shows_methods
+    puts Messages.help("queue count")
+
+  end
+
+
+
+  end
+
+    # load event_attendees.csv
+    # queue count should return 0
+    # find first_name John
+    # queue count should return 63
+    # queue clear
+    # queue count should return 0
+    # help should list the commands
+    # help queue count should explain the queue count function
+    # help queue print should explain the printing function
+    #
+
+end
+integration_test needs to be happy path first
