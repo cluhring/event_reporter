8e131380ee1eec992d726b7f0cb32d5da80dec54
Andrew Overby <andrewdoverby@gmail.com>
2014-11-18 20:43:51 -0700

    add to cli and messages

--- cli.rb
+++ cli.rb
@@ -3,6 +3,8 @@ require_relative 'cli'
 require_relative 'entry'
 require_relative 'entry_repository'
 #require_relative 'phone_book'
+#require_relative ...
+#require_relative ...
 require 'pry'
 #binding.pry
 
@@ -10,69 +12,96 @@ require 'pry'
 class CLI
 
   attr_reader :command, :messages, :instream, :outstream
+  attr_accessor :entries
 
-  def initialize(instream, outstream)
-    @command = ""
-    @messages = Messages.new
-    @instream = instream
+  def initialize
+    @command   = ""
+    @messages  = Messages.new
+    @instream  = instream
     @outstream = outstream
     @queue     = []
-    @entries   = []   #this is what parses the CSV file
+    @entries   = []   #this parses the CSV file
   end
 
   def finished?
-    @command == "quit"
+    start_with?("quit")
+  end
+
+  def queue_count?
+    @command == 'queue count'
+  end
+
+  def start_with?(word)
+    @command.split[0] == word
   end
 
   def start?
-    @command == "start"
-  end
-
-  def instructions?
-    @command == "help"
+    start_with?("start")
   end
 
   def load?
-    @command.start_with?("load")
+    start_with?("load")
   end
 
+
   def help?
-    @command.start_with?("help")
+    start_with?("help")
   end
 
   def start
-    outstream.puts messages.intro
+    puts messages.intro
     until finished?
-      outstream.puts messages.command_request
-      @command = instream.gets.strip.downcase
+      puts messages.command_request
+      @command = gets.chomp.downcase
       process_initial_commands
     end
+  end
+
+  def count_queue
+    @number_of_entries = @entries.count
+  end
+
+  def queue_print?
+    @command == "queue print"
+  end
+
+  def queue_print
+
+  end
+
+  def queue_clear?
+    @command == "queue clear"
+  end
+
+  def clears_queue
+    @entries.clear
   end
 
   def process_initial_commands
 
     case
-    when start?
-      outstream.puts "You've started."
-    when instructions?
-      outstream.puts messages.help
     when finished?
-      outstream.puts messages.ending
+      puts messages.ending
     when load?
-      outstream.puts messages.loads
+      puts messages.loads
       load_entries
     when help?
-      help_menu
-        #an if/else statement here?
-    when queue_count
-      #messages.results.count
-      outstream.puts messages.queue_count
-    when queue clear
-      outstream.puts messages.queue_clear
-    when queue print
-      outstream.puts messages.queue_print
-    #  when queue print by <attribute>
-    #   outstream.puts messages.queue_print...#what here?
+      if @command.split.length == 1
+        puts messages.commands.keys
+      else
+        help_menu
+      end
+    when queue_count?
+      count_queue
+      puts messages.queue_count(@number_of_entries)
+    when queue_clear?
+      clears_queue
+      puts messages.queue_clear
+    when queue print?
+      puts messages.queue_print
+
+    when queue print by <attribute>
+       puts messages.queue_print
     #
     # when queue save to <filename.csv.
     #   outstream.puts messages.queue_save_to # "#{filename}" ?
@@ -80,85 +109,80 @@ class CLI
     # when find <attribute> <criteria>
     #
     else
-      outstream.puts messages.not_a_valid_command
+      puts messages.not_a_valid_command
     end
   end
 
   def load_entries
-    filename = @command.split[1] || "event_attendees.csv"
-    CSV.foreach("./data/#{filename}", headers: true, header_converters: :symbol) do |row|
+    filename = @command.split[1] || "./data/event_attendees.csv"
+
+    CSV.foreach("#{filename}", headers: true, header_converters: :symbol) do |row|
       @entries << Entry.new(row)
     end
   end
 
   def help_menu
     case
-  #why have question marks in cli and not messages? help_find  vs. help_find?
     when help_find?
-      outstream.puts messages.help_find
+      puts messages.help_find
     when help_load?
-      outstream.puts messages.help_load
+      puts messages.help_load
     when help_queue?
-      outstream.puts messages.help_queue
+      puts messages.help_queue
     when help_queue_clear?
-      outstream.puts messages.help_queue_clear
+      puts messages.help_queue_clear
     when help_queue_count?
-      outstream.puts messages.help_queue_count
+      puts messages.help_queue_count
     when help_queue_print?
-      outstream.puts messages.help_queue_print
-    when help_save#how to do this?
-      outstream.puts messages.help_save
+      puts messages.help_queue_print
     end
   end
 
   private
 
   def help_queue_print?
-    @command.split[1..-1] == ["queue", "print"] # %w(queue print)
+    if @command == "queue print"
+      messages.help_queue_print
+    end
   end
 
-  def help_find #ask about search query
-    @command.split[0..1] ==
-    then puts "Select what to search by: id, reg_date, first_name, last_name, email_address,\n
-     home_phone, street, city, state, or zipcode."
+  def help_find? #ask next time about search query
+    if @command == "help find"
+      messages.help_find
+    end
      #need to find first_name John
   end
 
-  def help_load
-    command == %w(help load)
+  def help_load?
+    if @command == "help load"
+      messages.help_load
+    end
   end
 
-  def help_queue
-    command == %w(help queue)
+  def help_queue?
+    if @command == "help queue"
+      messages.help_queue
+    end
   end
 
-  def help_queue_clear
-    command == %w(queue clear)
+  def help_queue_clear?
+    if @command == "help queue clear"
+      messages.help_queue_clear
+    end
   end
 
-  def help_queue_count
-    command == %w(queue count)
-    #needs to explain how queue count works
+  def help_queue_count?
+    if @command == "help queue count"
+        messages.help_queue_count
+    end
   end
 
-  def help_queue_print
-    command == %w(queue print)
-    #needs to explain how printing function works
+  def help_queue_print?
+    if @command == "help queue print"
+        messages.help_queue_print
+   end
   end
-
-  def help_save
-    command == %w(help save to).csv   ||
-    command == %w(help save to).txt   ||
-    command == %w(help save to).json  ||
-    command == %w(help save to).xml   ||
-    command == %w(help save to).yml
-  end
-
-
 end
 
 
-
-
-end
-git
+cli = CLI.new.start
--- messages.rb
+++ messages.rb
@@ -1,57 +1,83 @@
 class Messages
-  def self.call
+  def response
     {
-      :intro => "Welcome to Event Reporter.",
-      :command_request => "What would you like to do?",
       :not_a_valid_command => "That's not a valid command. Try again or seek help.",
       :help => "Choose from among these help messages: find, load, queue,\n
-        queue clear, queue count, queue print."
+        queue clear, queue count, queue print.",
       :quit => "You have now quit.",
       :ending => "You have now closed Event Reporter."
     }
   end
 
-  def help
-    :help   #what else on this line?
+  def commands
+    {
+      'queue clear' => 'clears out queue',
+      'queue count' => 'gives number of items in queue',
+      'queue print' => 'shows items in queue',
+      'find' => 'finds item you searched and shows all information related to item',
+      'load <filename>' => 'loads file so you can access the data',
+      'queue' => 'does something with the queue'
+    }
   end
 
-  def find
+  def intro
+    "Welcome to Event Reporter."
+  end
 
+  def command_request
+    "What would you like to do?"
   end
 
   def loads
-    command == %w(loads)
-    #outstream.puts "#{file} loaded."
-    #what about help/help?||loads/loads?
+    "Loading your queue"
   end
 
-  def queue_count
-    command == %w(queue count)
-    #return 0
-    #return 63
-    #return "#{number}"
+  def queue_count(number_of_entries)
+    "There are #{number_of_entries} entries in the queue."
+  end
+
+
+  def help_queue_count
+    commands["queue count"]
+  end
+
+  def help_queue_print
+    commands["queue print"]
+  end
+
+  def help_find
+    commands["find"]
+  end
+
+  def help_load
+    commands["load <filename>"]
+  end
+
+  def help_queue_clear
+    commands["queue clear"]
+  end
+
+  def help_queue
+    commands["queue"]
   end
 
   def queue_clear
-    command == %w(queue clear)
+    "the queue is now cleared"
   end
 
   def queue_print
-    command == %w(queue print)
+    command["queue print"]
   end
-
-
 
   def command_request
     puts "What would you like to do?"
   end
 
   def not_a_valid_command
-    puts "That's not a valid command. Try again or seek help."
+    puts "That's not a valid command. Try again."
   end
 
   def file_not_found
-    outstream.puts "Think, then type. Pick another file name."
+    puts "Pick another file name."
   end
-end
 end
